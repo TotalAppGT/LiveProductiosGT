2a83972204e1a510bfe2c5ad82f161d153c2e1ea
TotalAppGT <totalappgt@gmail.com>
2026-08-10 21:30:37 -0600

    Fix presentacion: ahora como pagina Next.js en /presentacion con iframe al HTML

---
+++ src/app/presentacion/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+export default function Presentacion() {
+  return (
+    <iframe src="/presentacion.html" style={{ width: "100%", height: "100vh", border: "none" }} title="Presentación Live Productions GT" />
+  );
+}
