7bc6c95a335347aae348dbc8e3d8d8458b03a21b
TotalAppGT <totalappgt@gmail.com>
2026-08-10 20:00:28 -0600

    CRITICAL: next.config.ts instrumentationHook=true - el cron jamas se iniciaba

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "**" },
     ],
   },
+  experimental: {
+    instrumentationHook: true,
+  },
 };
 
 export default nextConfig;
